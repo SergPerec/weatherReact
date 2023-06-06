874b3d16cc3c8fbc9ea6db2cff22b1f9a4469cc6
Stro_Serg <Prahatuc@mail.ru>
2023-06-06 15:45:22 +0300

    add components Tabs

--- src/components/Weather/Main/Tabs.tsx
+++ src/components/Weather/Main/Tabs.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import BlockNowNew from './Tabs/BlockNowNew/BlockNowNew';
 import BlockDetails from './Tabs/BlockDetails/BlockDetails';
 import BlockForecast from './Tabs/BlockForecast/BlockForecast';
+import './TabsStyle.css';
 
 const Tabs = () => {
   const tabs = {
@@ -24,6 +25,20 @@ const Tabs = () => {
         <div className={`tab-content ${activeTab === tabs.forecast ? 'active' : ''}`}>
           <BlockForecast />
         </div>
+      </div>
+      <div className="block-links">
+        <button className={`tab-links ${activeTab === tabs.now ? 'active' : ''}`}
+        onClick={() => (activeTab !== tabs.now ? setActiveTab(tabs.now): null)}>
+            Сейчас
+        </button>
+        <button className={`tab-links ${activeTab === tabs.details ? 'active' : ''}`}
+        onClick={() => (activeTab !== tabs.details ? setActiveTab(tabs.details): null)}>
+          Детали
+        </button>
+        <button className={`tab-links ${activeTab === tabs.forecast ? 'active' : ''}`}
+        onClick={() => (activeTab !== tabs.forecast ? setActiveTab(tabs.forecast): null)}>
+          Прогноз
+        </button>
       </div>
     </div>
   );
